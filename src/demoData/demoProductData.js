5c1a077f408ea83b3ac722ad7a81bac2c70a7d82
Ogbuagu Chijioke <ogbuaguchijioke4@gmail.com>
2024-04-10 18:34:08 +0100

    worked on products page

--- src/demoData/demoProductData.js
+++ src/demoData/demoProductData.js
@@ -3,36 +3,188 @@ export const demoProductData = [
         "productId": "43748591dhfh--2",
         "productTitle": "Lira Earrings",
         "productImg": "/images/ear1.png",
-        "productPrice":"20,000"
+        "demoPictures":{
+            "img1": "/images/ear1.png",
+            "img2": "/images/ear1.png",
+            "img3": "/images/ear1.png",
+            "img4": "/images/ear1.png",
+        },
+        "productDescription":"           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis quidem itaque mollitia autem? Saepe asperiores ipsa cum doloremque, temporibus reprehenderit perspiciatis odit atque aliquam quis dicta possimus cupiditate magnam recusandae eveniet quidem. ",
+        "message": {
+            "type": true,
+            "message": "-21%"
+        },
+        "productPrice":"20,000",
+        "sku": 10,
+        "reviews":{
+            "rating": 5,
+            "numberOfReviews": 78,
+            "reviewsLink": "/review/#ute56"
+        },
+        "categories": ["Fashion", "Style"],
+        "similarProducts": ["458584hf74h3--2","dshdns383993dh--2","37474njdhibnx--2"],
+        "AdditionalInformation": {
+            "weight": 0.3,
+            "dimensions": "15 x 10 x 1",
+            "colours": ["black", "brown", "white"],
+            "material": "metal"
+        }
+
     },
     {
         "productId": "458584hf74h3--2",
         "productTitle": "Hal Earrings",
         "productImg": "/images/ear2.png",
-        "productPrice":"25,000"
+        "demoPictures":{
+            "img1": "/images/ear2.png",
+            "img2": "/images/ear2.png",
+            "img3": "/images/ear2.png",
+            "img4": "/images/ear2.png",
+        },
+        "productDescription":"           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis quidem itaque mollitia autem? Saepe asperiores ipsa cum doloremque, temporibus reprehenderit perspiciatis odit atque aliquam quis dicta possimus cupiditate magnam recusandae eveniet quidem.",
+        "message": {
+            "type": false,
+            "message": ""
+        },
+        "productPrice":"25,000",
+        "sku": 130,
+        "reviews":{
+            "rating": 3.5,
+            "numberOfReviews": 23,
+            "reviewsLink": "/review/#svv98d"
+        },
+        "categories": ["Fashion"],
+        "similarProducts": ["dshdns383993dh--2","4348cnfddfndi2--2","37474njdhibnx--2"],
+        "AdditionalInformation": {
+            "weight": 0.67,
+            "dimensions": "12 x 10 x 3",
+            "colours": ["black", "purple", "white", "silver", "gold"],
+            "material": "gold"
+        }
+        
     },
     {
         "productId": "37474njdhibnx--2",
         "productTitle": "Kaede Hair Pin Set Of 3 ",
         "productImg": "/images/ear3.png",
-        "productPrice":"30,000"
+        "demoPictures":{
+            "img1": "/images/ear3.png",
+            "img2": "/images/ear3.png",
+            "img3": "/images/ear3.png",
+            "img4": "/images/ear3.png",
+        },
+        "productDescription":"            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis quidem itaque mollitia autem? Saepe asperiores ipsa cum doloremque, temporibus reprehenderit perspiciatis odit atque aliquam quis dicta possimus cupiditate magnam recusandae eveniet quidem.",
+        "message": {
+            "type": false,
+            "message": ""
+        },
+        "productPrice":"30,000",
+        "sku": 7,
+        "reviews":{
+            "rating":4 ,
+            "numberOfReviews": 35,
+            "reviewsLink": "/review/#kh56j8"
+        },
+        "categories": ["season", "accessories"],
+        "similarProducts": ["374849033nccss--2","43748591dhfh--2","4348cnfddfndi2--2"],
+        "AdditionalInformation": {
+            "weight": 0.17,
+            "dimensions": "7 x 9 x 1",
+            "colours": [ "silver", "gold"],
+            "material": "silver"
+        }
     },
     {
         "productId": "4348cnfddfndi2--2",
         "productTitle": "Hair Pin Set of 3",
         "productImg": "/images/ear4.png",
-        "productPrice":"30,000"
+        "demoPictures":{
+            "img1": "/images/ear4.png",
+            "img2": "/images/ear4.png",
+            "img3": "/images/ear4.png",
+            "img4": "/images/ear4.png",
+        },
+        "productDescription":"           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis quidem itaque mollitia autem? Saepe asperiores ipsa cum doloremque, temporibus reprehenderit perspiciatis odit atque aliquam quis dicta possimus cupiditate magnam recusandae eveniet quidem.",
+        "message": {
+            "type": false,
+            "message": ""
+        },
+        "productPrice":"30,000",
+        "sku": 16,
+        "reviews":{
+            "rating":5 ,
+            "numberOfReviews": 67,
+            "reviewsLink": "/review/#dtya65"
+        },
+        "categories": ["season", "style"],
+        "similarProducts": ["374849033nccss--2","dshdns383993dh--2","37474njdhibnx--2"],
+        "AdditionalInformation": {
+            "weight": 1.2,
+            "dimensions": "10 x 20 x 1",
+            "colours": [ "black"],
+            "material": "coper"
+        }
     },
     {
         "productId": "dshdns383993dh--2",
         "productTitle": "Plaine Necklace",
         "productImg": "/images/ear5.png",
-        "productPrice":"19,000"
+        "demoPictures":{
+            "img1": "/images/ear5.png",
+            "img2": "/images/ear5.png",
+            "img3": "/images/ear5.png",
+            "img4": "/images/ear5.png",
+        },
+        "productDescription":"           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis quidem itaque mollitia autem? Saepe asperiores ipsa cum doloremque, temporibus reprehenderit perspiciatis odit atque aliquam quis dicta possimus cupiditate magnam recusandae eveniet quidem.",
+        "message": {
+            "type": true,
+            "message": "Sold Out"
+        },
+        "productPrice":"19,000",
+        "sku": 0,
+        "reviews":{
+            "rating":5 ,
+            "numberOfReviews": 198,
+            "reviewsLink": "/review/#asj389"
+        },
+        "categories": ["season", "style", "fashion"],
+        "similarProducts": ["458584hf74h3--2","43748591dhfh--2","37474njdhibnx--2"],
+        "AdditionalInformation": {
+            "weight": 0.9,
+            "dimensions": "10 x 10 x 1",
+            "colours": [ "black", "brown", "silver"],
+            "material": "metal"
+        }
     },
     {
         "productId": "374849033nccss--2",
         "productTitle": "Yuki Hair Pin Set of 3",
         "productImg": "/images/ear6.png",
-        "productPrice":"29,000"
+        "demoPictures":{
+            "img1": "/images/ear6.png",
+            "img2": "/images/ear6.png",
+            "img3": "/images/ear6.png",
+            "img4": "/images/ear6.png",
+        },
+        "productDescription":"           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis quidem itaque mollitia autem? Saepe asperiores ipsa cum doloremque, temporibus reprehenderit perspiciatis odit atque aliquam quis dicta possimus cupiditate magnam recusandae eveniet quidem.",
+        "message": {
+            "type": false,
+            "message": ""
+        },
+        "productPrice":"29,000",
+        "sku": 12,
+        "reviews":{
+            "rating":4.5 ,
+            "numberOfReviews": 19,
+            "reviewsLink": "/review/#skss392"
+        },
+        "categories": ["season"],
+        "similarProducts": ["37474njdhibnx--2","458584hf74h3--2","dshdns383993dh--2"],
+        "AdditionalInformation": {
+            "weight": 0.2,
+            "dimensions": "11 x 15 x 2",
+            "colours": [  "brown", "silver"],
+            "material": "metal"
+        }
     }
 ]
